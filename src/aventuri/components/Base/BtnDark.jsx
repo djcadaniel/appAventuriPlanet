5897160e7b82337199c61f8ed9710bc172bf8f0a
djcadaniel <djcadaniel@gmail.com>
2024-03-20 20:16:12 -0500

    add min-width

--- src/aventuri/components/Base/BtnDark.jsx
+++ src/aventuri/components/Base/BtnDark.jsx
@@ -1,10 +1,8 @@
-import React from 'react'
-
 export const BtnDark = () => {
   return (
     <>
-      <button className='bg-blue-800 px-5 py-2 rounded-md'>
-        <span className='text-white font-semibold'>Read More</span>
+      <button className='bg-blue-800 hover:bg-white px-5 py-2 rounded-md text-white hover:text-blue-800 duration-300'>
+        <span className=' font-semibold '>Read More</span>
       </button>
     </>
   )
--- src/aventuri/components/Base/Logo.jsx
+++ src/aventuri/components/Base/Logo.jsx
@@ -4,7 +4,7 @@ import logo from '/img/desktop/logo.png'
 export const Logo = () => {
   return (
     <Link className='' to='/'>
-      <img className="h-10" src={logo} alt="logo del sitio web" />
+      <img className="h-14 md:h-24" src={logo} alt="logo del sitio web" />
     </Link>
   )
 }
