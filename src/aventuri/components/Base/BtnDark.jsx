da287782a7880937bf289b1d7529c0bf643932ac
djcadaniel <djcadaniel@gmail.com>
2024-08-16 12:30:48 -0500

    changing of color to read more

--- src/aventuri/components/Base/BtnDark.jsx
+++ src/aventuri/components/Base/BtnDark.jsx
@@ -1,7 +1,7 @@
 export const BtnDark = () => {
   return (
     <>
-      <button className='text bg-azul/85 hover:bg-white/85 px-5 py-2 rounded-md text-white hover:text-blue-800 duration-300'>
+      <button className='text bg-azul/85 hover:bg-white/85 px-5 py-2 rounded-md text-[white!important] hover:text-blue-800 duration-300'>
         <span className=' font-semibold uppercase'>Leer más</span>
       </button>
     </>
