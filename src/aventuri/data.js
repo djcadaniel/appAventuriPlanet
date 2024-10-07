c08b15cfa394688bc2a510f760b828612aad74b1
djcadaniel <djcadaniel@gmail.com>
2024-10-07 17:30:26 -0500

    add slider

---
+++ src/aventuri/data.js
@@ -0,0 +1,53 @@
+import carlos from '/img/desktop/logo.png'
+import aldo from '/img/desktop/logo.png'
+
+export const dataComments= [
+  {
+    nombre: 'Carlos Córdova',
+    carrera: 'Full Stack - developer',
+    description: 'Daniel me ayudó a mejorar mis habilidades como desarrollador front-end. Siempre está dispuesto a ayudar y compartir sus conocimientos con los demás.',
+    imgUser: carlos
+  },
+  {
+    nombre: 'Aldo Ordoñez',
+    carrera: 'Full Stack - developer',
+    description: 'Daniel meeee ayudó a mejorar mis habilidades como desarrollador front-end. Siempre está dispuesto a ayudar y compartir sus conocimientos con los demás.',
+    imgUser: aldo
+  },
+  {
+    nombre: 'Kheny Gutierres',
+    carrera: 'Full Stack - developer',
+    description: 'Daniel meeee ayudó a mejorar mis habilidades como desarrollador front-end. Siempre está dispuesto a ayudar y compartir sus conocimientos con los demás.',
+    imgUser: aldo
+  }, 
+  {
+    nombre: 'Kheny Gutierres',
+    carrera: 'Full Stack - developer',
+    description: 'Daniel meeee ayudó a mejorar mis habilidades como desarrollador front-end. Siempre está dispuesto a ayudar y compartir sus conocimientos con los demás.',
+    imgUser: aldo
+  }, 
+  {
+    nombre: 'Kheny Gutierres',
+    carrera: 'Full Stack - developer',
+    description: 'Daniel meeee ayudó a mejorar mis habilidades como desarrollador front-end. Siempre está dispuesto a ayudar y compartir sus conocimientos con los demás.',
+    imgUser: aldo
+  }, 
+  {
+    nombre: 'Kheny Gutierres',
+    carrera: 'Full Stack - developer',
+    description: 'Daniel meeee ayudó a mejorar mis habilidades como desarrollador front-end. Siempre está dispuesto a ayudar y compartir sus conocimientos con los demás.',
+    imgUser: aldo
+  }, 
+  {
+    nombre: 'Kheny Gutierres',
+    carrera: 'Full Stack - developer',
+    description: 'Daniel meeee ayudó a mejorar mis habilidades como desarrollador front-end. Siempre está dispuesto a ayudar y compartir sus conocimientos con los demás.',
+    imgUser: aldo
+  }, 
+  {
+    nombre: 'Kheny Gutierres',
+    carrera: 'Full Stack - developer',
+    description: 'Daniel meeee ayudó a mejorar mis habilidades como desarrollador front-end. Siempre está dispuesto a ayudar y compartir sus conocimientos con los demás.',
+    imgUser: aldo
+  }, 
+]
--- src/aventuri/pages/Home/components/Store.jsx
+++ src/aventuri/pages/Home/components/Store.jsx
@@ -1,131 +1,79 @@
-import { BtnPlay } from '../../../components/Base/BtnPlay';
-import iconStar from '/img/mobile/star.png';
-import girlMobile from '/img/mobile/girlMobile.png';
-import { TitleComponentsHome } from './Common/TitleComponentsHome';
-import "react-image-gallery/styles/css/image-gallery.css";
-import ImageGallery from "react-image-gallery";
-import { useEffect, useRef, useState } from 'react';
-import { list } from 'postcss';
-import { Camp } from './Camp';
-import bgChildrenMobile from '/img/mobile/childrenMobile.png';
-import bgChildrenDesktop from '/img/desktop/childrenDesktop.png';
-import { useMediaQuery } from 'react-responsive'
+import React, { useRef, useState } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
 
-const images = [
-  {
-    id: 1,
-    original: "https://picsum.photos/id/1018/1000/600",
-    thumbnail: "https://picsum.photos/id/1018/250/150"
-  },
-  {
-    id: 2,
-    original: "https://picsum.photos/id/1015/1000/600",
-    thumbnail: "https://picsum.photos/id/1015/250/150"
-  },
-  {
-    id: 3,
-    original: "https://picsum.photos/id/237/1000/600",
-    thumbnail: "https://picsum.photos/id/237/250/150"
-  },
-  {
-    id: 4,
-    original: "https://picsum.photos/id/238/1000/600",
-    thumbnail: "https://picsum.photos/id/238/250/150"
-  },
-  {
-    id: 5,
-    original: "https://picsum.photos/id/239/1000/600",
-    thumbnail: "https://picsum.photos/id/239/250/150"
-  },
-  {
-    id: 6,
-    original: "https://picsum.photos/id/240/1000/600",
-    thumbnail: "https://picsum.photos/id/240/250/150"
-  },
-] 
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/grid';
+import 'swiper/css/pagination';
+
+import '../../../../assets/styles/global.css'
+// import required modules
+import { Grid, Pagination } from 'swiper/modules';
+
+import iconStar from '/img/mobile/star.png';
+import { TitleComponentsHome } from './Common/TitleComponentsHome';
 
 export const Store = () => {
 
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const [selectedImage, setSelectedImage] = useState(images[0].original)
-  // const [storeImages, setStoreImages] = useState([images[0].original, images[1].original])
-  const [storeImages, setStoreImages] = useState([images[0].original])
-
-  const isBigScreen = useMediaQuery({ query: '(max-width:1400px)' })
-  const isDesktop = useMediaQuery({query: '(max-width: 1000px)'  })
-  const isTable = useMediaQuery({ query: '(max-width:768px)' })
-  const isMobile = useMediaQuery({ query: '(max-width:768px)' })
-
-  const newImage = (index, images, next=true, cant) => {
-    const condition = next ? (currentIndex < images.length - 1) : (currentIndex > 0);
-    const nextIndex = next ? (condition ? currentIndex + cant : 0) : (condition ? currentIndex - cant : images.length - 1)
-    if(nextIndex >= images.length){
-      setCurrentIndex(0)
-      setStoreImages([])
-    }else{
-      setSelectedImage(images[nextIndex].original)
-      setCurrentIndex(nextIndex)
-      setStoreImages(prev => {
-        // return [...prev, images[nextIndex].original, images[nextIndex + 1].original]
-        return [...prev, images[nextIndex].original]
-      })
-    }
-  }
-  // setImagesToShow()
-
-  // const previous = () => {
-  //   newImage(currentIndex, images, false, 1)
-  // }
-  // const previous = useMediaQuery(
-  //   {isMobile},
-  //   newImage(currentIndex, images, false, 1)
-  // )
-  // const next = useMediaQuery(
-    //   {isMobile},
-    //   newImage(currentIndex, images, false, 1)
-    // )
-    const previous = () =>{
-      if(isMobile){
-         newImage(currentIndex, images, false, 1)
-      }
-    }
-    const next = () =>{
-      if(isMobile){
-         newImage(currentIndex, images, true, 2)
-      }
-    }
   
-  console.log(currentIndex)
-  console.log(selectedImage)
-  console.log(storeImages)
   return (
-    <div className="relative w-full h-full p-0 container">
+    <div className="relative w-full h-[100vh] p-0 container">
       <div className='px-5 pt-10 pb-8'>
         <TitleComponentsHome>
           <img src={iconStar} alt="" className=' h-10 md:h-12 inline-block mr-2'/>
           <h1 className="title-p1 inline-block align-bottom">AVENTURI TIENDA</h1>
         </TitleComponentsHome>
       </div>
-
-      <div className=' w-full h-[calc(100%-9rem)] flex flex-col'>
-        <div className='w-full h-[50%] rounded-3xl py-6 px-14'>
-            <div className='absolute top-[45%] left-0' onClick={previous} >prev</div>
-            <div className='absolute top-[45%] right-0' onClick={next} >next</div>
-            {storeImages?.map( item => (
-              <div key={item} className='w-full h-full'>
-                <img src={item} alt="image del slide" className='h-full w-full object-cover' /> 
-              </div>
-            ))}
-        </div>
-        <div
-            className='relative h-[45%] w-full bg-cover bg-center bg-no-repeat'
-            style={{ backgroundImage: `url(${bgChildrenMobile})` }}
+      <div className=' w-full h-[calc(100%-9rem)] bg-gray-500'>
+        <Swiper
+            slidesPerView={3}
+            grid={{
+              rows: 2,
+              fill: 'row'
+            }}
+            spaceBetween={30}
+            pagination={{
+              clickable: true,
+            }}
+            breakpoints={{
+              1: {
+                slidesPerView: 1,
+                spaceBetween: 20
+              },
+              520: {
+                slidesPerView: 2,
+                spaceBetween: 20
+              },
+              1024: {
+                slidesPerView: 3,
+                spaceBetween: 40
+              },
+              1200: {
+                slidesPerView: 5,
+                spaceBetween: 50
+              }
+            }}
+            modules={[Grid, Pagination]}
+            className="mySwiper"
           >
-          <figure className='absolute top-0 left-0 right-0 bottom-0 w-full h-full'>
-            <img src={bgChildrenDesktop} alt="" className='hidden md:block w-full h-full object-center '/>
-            <img src={bgChildrenMobile} alt="" className='block md:hidden w-full h-full object-center  '/>
-          </figure>
-        </div>
+            <SwiperSlide>Slide 1</SwiperSlide>
+            <SwiperSlide>Slide 2</SwiperSlide>
+            <SwiperSlide>Slide 3</SwiperSlide>
+            <SwiperSlide>Slide 4</SwiperSlide>
+            <SwiperSlide>Slide 5</SwiperSlide>
+            <SwiperSlide>Slide 6</SwiperSlide>
+            <SwiperSlide>Slide 7</SwiperSlide>
+            <SwiperSlide>Slide 8</SwiperSlide>
+            <SwiperSlide>Slide 9</SwiperSlide>
+            <SwiperSlide>Slide 10</SwiperSlide>
+            <SwiperSlide>Slide 11</SwiperSlide>
+            <SwiperSlide>Slide 12</SwiperSlide>
+            <SwiperSlide>Slide 13</SwiperSlide>
+            <SwiperSlide>Slide 14</SwiperSlide>
+            <SwiperSlide>Slide 15</SwiperSlide>
+            <SwiperSlide>Slide 16</SwiperSlide>
+          </Swiper>
       </div>
     </div>
   )
